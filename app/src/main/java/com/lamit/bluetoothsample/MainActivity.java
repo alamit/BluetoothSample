4a915e6a0417bf3efab88ea4a1737f2b485e9192
alamit <adrien.lamit@me.com>
2017-01-13 15:02:46 +0100

    fix data collection when no bluetooth devices

--- app/src/main/java/com/lamit/bluetoothsample/MainActivity.java
+++ app/src/main/java/com/lamit/bluetoothsample/MainActivity.java
@@ -11,14 +11,14 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
     TextView myLabel;
     BluetoothAdapter mBluetoothAdapter;
     BluetoothSocket mmSocket;
@@ -32,75 +32,66 @@ public class MainActivity extends Activity
     volatile boolean stopWorker;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button openButton = (Button)findViewById(R.id.open);
-        Button closeButton = (Button)findViewById(R.id.close);
-        myLabel = (TextView)findViewById(R.id.label);
+        Button openButton = (Button) findViewById(R.id.open);
+        Button closeButton = (Button) findViewById(R.id.close);
+        myLabel = (TextView) findViewById(R.id.label);
 
         //Open Button
-        openButton.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                try
-                {
-                    findBT();
-                    openBT();
-                }
-                catch (IOException ex) { }
+        openButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                findBT();
             }
         });
 
         //Close button
-        closeButton.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                try
-                {
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
                     closeBT();
+                    myLabel.setText("Bluetooth connection closed by user.");
+                } catch (IOException ex) {
+                    myLabel.setText("Failed to close Bluetooth due to : " + ex.getMessage());
+
                 }
-                catch (IOException ex) { }
             }
         });
     }
 
-    void findBT()
-    {
+    void findBT() {
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(mBluetoothAdapter == null)
-        {
+        if (mBluetoothAdapter == null) {
             myLabel.setText("No bluetooth adapter available");
             return;
         }
 
-        if(!mBluetoothAdapter.isEnabled())
-        {
+        if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBluetooth, 0);
         }
 
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        if(pairedDevices.size() > 0)
-        {
-            for(BluetoothDevice device : pairedDevices)
-            {
-                if(device.getName().equals("HC-05"))
-                {
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                if (device.getName().equals("HC-05")) {
                     mmDevice = device;
-                    break;
+                    myLabel.setText("Bluetooth Device Found, starting data collection.");
+                    try {
+                        openBT();
+                    } catch (IOException e) {
+                        myLabel.setText("Data collection failed due to : " + e.getMessage());
+                    }
+                    return;
                 }
             }
         }
-        myLabel.setText("Bluetooth Device Found");
+        myLabel.setText("Search complete, no HC-05 Bluetooth found.");
     }
 
-    void openBT() throws IOException
-    {
+    void openBT() throws IOException {
         UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
         mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
         mmSocket.connect();
@@ -112,54 +103,40 @@ public class MainActivity extends Activity
         myLabel.setText("Bluetooth Opened");
     }
 
-    void beginListenForData()
-    {
+    void beginListenForData() {
         final Handler handler = new Handler();
         final byte delimiter = 10; //This is the ASCII code for a newline character
 
         stopWorker = false;
         readBufferPosition = 0;
         readBuffer = new byte[1024];
-        workerThread = new Thread(new Runnable()
-        {
-            public void run()
-            {
-                while(!Thread.currentThread().isInterrupted() && !stopWorker)
-                {
-                    try
-                    {
+        workerThread = new Thread(new Runnable() {
+            public void run() {
+                while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+                    try {
                         int bytesAvailable = mmInputStream.available();
-                        if(bytesAvailable > 0)
-                        {
+                        if (bytesAvailable > 0) {
                             byte[] packetBytes = new byte[bytesAvailable];
                             mmInputStream.read(packetBytes);
-                            for(int i=0;i<bytesAvailable;i++)
-                            {
+                            for (int i = 0; i < bytesAvailable; i++) {
                                 byte b = packetBytes[i];
-                                if(b == delimiter)
-                                {
+                                if (b == delimiter) {
                                     byte[] encodedBytes = new byte[readBufferPosition];
                                     System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
                                     final String data = new String(encodedBytes, "US-ASCII");
                                     readBufferPosition = 0;
 
-                                    handler.post(new Runnable()
-                                    {
-                                        public void run()
-                                        {
+                                    handler.post(new Runnable() {
+                                        public void run() {
                                             myLabel.setText(data);
                                         }
                                     });
-                                }
-                                else
-                                {
+                                } else {
                                     readBuffer[readBufferPosition++] = b;
                                 }
                             }
                         }
-                    }
-                    catch (IOException ex)
-                    {
+                    } catch (IOException ex) {
                         stopWorker = true;
                     }
                 }
@@ -169,8 +146,7 @@ public class MainActivity extends Activity
         workerThread.start();
     }
 
-    void closeBT() throws IOException
-    {
+    void closeBT() throws IOException {
         stopWorker = true;
         mmOutputStream.close();
         mmInputStream.close();
